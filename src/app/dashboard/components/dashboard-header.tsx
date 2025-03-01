25ae7b6151b8e847445e120ffb757d09adf29f77
Alyxson Marques <alyxson.marques0@gmail.com>
2025-02-28 21:59:03 -0300

    Changed divs for semantic tags

--- src/app/dashboard/components/dashboard-header.tsx
+++ src/app/dashboard/components/dashboard-header.tsx
@@ -13,7 +13,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 export function DashboardHeader() {
   return (
     <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2 px-4">
+      <section className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb>
@@ -27,10 +27,10 @@ export function DashboardHeader() {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-      </div>
-      <div className="px-4">
+      </section>
+      <section className="px-4">
         <ModeToggle />
-      </div>
+      </section>
     </header>
   );
 }
--- src/components/CentralizedView.tsx
+++ src/components/CentralizedView.tsx
@@ -1,5 +1,5 @@
 export function CentralizedView({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">{children}</div>
+    <main className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">{children}</main>
   );
 }
